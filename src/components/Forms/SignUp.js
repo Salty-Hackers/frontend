5d9a250296c5a5f829cde539442c577a1fd4448c
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-09-19 19:42:33 -0400

    just basic login and sign up components, including some added styling. Will be working wirh Tzong-lian on routes, yup, axios next

---
+++ src/components/Forms/SignUp.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import './Login.css'
+
+
+
+export default function SignUp() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+  
+  
+    const handleSubmit = (event) => {
+      console.log(`
+        Email: ${email}
+        Password: ${password}
+  
+      `);
+  
+      event.preventDefault();
+    }
+  
+    return (
+        <>
+      
+      <form onSubmit={handleSubmit} id="loginform">
+        <h1>Create Account</h1>
+  
+        <label>
+          <p>Email:</p>
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required />
+        </label>
+  
+        <label>
+          <p>Create Password:</p>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required />
+        </label>
+
+        <label>
+        <p>Re-enter Password:</p>
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required />
+      </label>
+  
+       
+  
+  
+        <button>Submit</button>
+      </form>
+   
+     </>
+    );
+  }
