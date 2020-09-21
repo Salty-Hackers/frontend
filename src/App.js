8e5e0b2b0bc8fe6535cdc65faeff2cd136b372a9
Matthew Mulford <mhmulford0@gmail.com>
2020-09-21 10:21:37 -0500

    commit cleanup

--- src/App.js
+++ src/App.js
@@ -3,28 +3,13 @@ import React from 'react';
 import './App.css';
 import { UserProvider } from "./components/contexts/UserContext";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import Login from './components/Forms/Login'
-import SignUp from "./components/Forms/SignUp";
 
 function App() {
   return (
-    <Router>
-      <UserProvider>
+
         <div className="App">
           <h1>Saltiest Hacker</h1>
         </div>
-
-        <Route path="/login">
-          <Login />
-        </Route>
-
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-      </UserProvider>
-    </Router>
   );
 }
 
