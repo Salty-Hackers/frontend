caecf41d4fb6637cb00834079153237b262b012e
Matthew Mulford <mhmulford0@gmail.com>
2020-09-22 15:57:16 -0500

    filtered comments

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,5 @@
 //import { axiosWithAuth } from "../utils/axiosWithAuth";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 // This card is used to:
 // 1. Render the users saves
@@ -9,8 +9,21 @@ import { useComments } from "./contexts/UserContext";
 
 function UserProfile() {
     let { id } = useParams();
+ 
+    const [comment, setComment] = useState([])
     const getComments = useComments();
-    const userComments = getComments.filter(commentId => commentId === id)
+    
+    useEffect(() => {
+        setComment(getComments)  
+    }, [getComments])
+
+
+    
+    const result = comment.filter(com => com.user_id == id)
+    console.log(result)
+
+
+    
 
 
     // need to make a axios call to render the users saved cards
@@ -19,11 +32,11 @@ function UserProfile() {
     // need to add mark-up and some styling
     return (
         <>
-            <h1> Welcome to your Favorite Comments </h1>
+            {/* <h1> Welcome to your Favorite Comments </h1>
             <h5> Here you can view and delete comment cards </h5>
             <p>User ID {id}</p>
             {userComments.map(person =>
-                <p> {person.id} </p>)}
+                <p> {person.id} </p>)} */}
         </>
     )
 
