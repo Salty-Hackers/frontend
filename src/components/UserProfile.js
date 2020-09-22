6a439554bb6d17d617b64f5b81a8809ff28e74f8
Jennifer Stewart <jstewart8053@gmail.com>
2020-09-22 00:20:54 -0500

    made updates to Dashboard, Card, and UserProfile components. Added axios calls, state, and some functionality and lots of comments

---
+++ src/components/UserProfile.js
@@ -0,0 +1,25 @@
+//import { axiosWithAuth } from "../utils/axiosWithAuth";
+import React from "react";
+
+// This card is used to:
+// 1. Render the users saves
+// 2. Allow the user to delete saved cards
+
+function UserProfile() {
+    // need to make a axios call to render the users saved cards
+    // need to make a delete function to delete saved cards
+
+
+
+
+    // need to add mark-up and some styling
+    return (
+        <>
+            <h1> Welcome to your Favorite Comments </h1>
+            <h5> Here you can view and delete comment cards </h5>
+        </>
+    )
+
+}
+
+export default UserProfile;
