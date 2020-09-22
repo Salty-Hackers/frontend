cb9db0a7521c8df52a695af0a765aea08437e926
Jennifer Stewart <jstewart8053@gmail.com>
2020-09-22 16:47:50 -0500

    added map to userProfile so comment and negativity display

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -8,31 +8,34 @@ import { useParams } from "react-router-dom";
 import { useComments } from "./contexts/UserContext";
 
 function UserProfile() {
-  let { id } = useParams();
+    let { id } = useParams();
 
-  const [comment, setComment] = useState([]);
-  const getComments = useComments();
+    const [comment, setComment] = useState([]);
+    const getComments = useComments();
 
-  useEffect(() => {
-    setComment(getComments);
-  }, [getComments]);
+    useEffect(() => {
+        setComment(getComments);
+    }, [getComments]);
 
-  const result = comment.filter((com) => com.user_id == id);
-  console.log(result);
+    const result = comment.filter((com) => com.user_id == id);
+    console.log(result);
 
-  // need to make a axios call to render the users saved cards
-  // need to make a delete function to delete saved cards
+    // need to make a axios call to render the users saved cards
+    // need to make a delete function to delete saved cards
 
-  // need to add mark-up and some styling
-  return (
-    <>
-      {/* <h1> Welcome to your Favorite Comments </h1>
+    // need to add mark-up and some styling
+    return (
+        <>
+            <h1> Welcome to your Favorite Comments </h1>
             <h5> Here you can view and delete comment cards </h5>
             <p>User ID {id}</p>
-            {userComments.map(person =>
-                <p> {person.id} </p>)} */}
-    </>
-  );
+            {result.map(person =>
+                <div key >
+                    <p> {person.comment} </p>
+                    <p> {person.negativity} </p>
+                </div>)}
+        </>
+    );
 }
 
 export default UserProfile;
