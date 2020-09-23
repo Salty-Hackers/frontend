e5234a059fac29bcc5bbdfba4a079917beb5f837
Matthew Mulford <mhmulford0@gmail.com>
2020-09-23 14:00:28 -0500

    rebase

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,15 @@
 
 import React, { useEffect, useState } from "react";
 
-<<<<<<< HEAD
-
-=======
 function Card(props) {
     const {comment} = props
->>>>>>> 21429a2... info added to cards
     return (
         <div className="card" >
             <h3 className="cardTitles">Comment: </h3> <br />
             {comment.comment}<br /><br/>
 
-<<<<<<< HEAD
-            <h3 className="cardTitles">Rank: </h3>
-            {rank.filter(rankers =>
-                <p className="cardData"> {rankers.negativity}</p>)}
-            <br />
-            <h3 className="cardTitles">User: </h3>
-            {user.map(person =>
-                <p className="cardData"> {person.email} </p>)}
-            <br />
-            <h3 className="cardTitles">Saltiest Comment: </h3>
-            {comments.filter(comment =>
-                <p className="cardData" id="commentText">
-                    {comment.comments}
-                </p>)}
-            <br />
-            <button className="cardButton" onClick >delete </button>
-=======
             <h3 className="cardTitles">User: </h3> 
             {comment.user_id}
->>>>>>> 21429a2... info added to cards
         </div>
 
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,49 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
 
-<<<<<<< HEAD
-//  import { axiosWithAuth } from "../utils/axiosWithAuth";
-// The dashboard is used to:
-// 1. Display the user saved Cards
-// 2. Allow the user to delete the saved Cards
-=======
 import { useGetSavedCommentContext } from "./contexts/UserContext";
->>>>>>> 21429a2... info added to cards
 
 
 
 function Dashboard() {
-<<<<<<< HEAD
-
-=======
   const getSavedComments = useGetSavedCommentContext();
 
   console.log(getSavedComments);
   // need to make axios function to populate the drop-down list or maybe change to a search bar instead ???
->>>>>>> 21429a2... info added to cards
 
 
   return (
     <>
       <div className="dashHeader">
-<<<<<<< HEAD
-        <div className="profileLink">
-
-          <Link to="/comments">All Comments</Link>
-          <Route path="/comments" />
-
-        </div>
-
-
-      </div>
-      <UserProfile />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-=======
  
 
         <h1> Saltiest Hacker </h1>
@@ -56,7 +27,6 @@ function Dashboard() {
       {getSavedComments.map(comments => {
         return <Card comment={comments} />
       })}
->>>>>>> 21429a2... info added to cards
     </>
   );
 }
