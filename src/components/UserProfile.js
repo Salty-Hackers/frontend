c7f4b81efe048c9c3b88a51fd6d238cc1c8fcdbc
Matthew Mulford <mhmulford0@gmail.com>
2020-09-23 14:21:52 -0500

    more changes

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,5 @@
-//import { axiosWithAuth } from "../utils/axiosWithAuth";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-// This card is used to:
-// 1. Render the users saves
-// 2. Allow the user to delete saved cards
 
 import { useComments } from "./contexts/UserContext";
 
@@ -20,10 +16,7 @@ function UserProfile() {
     const result = comment.filter((com) => com.user_id == id);
     console.log(result);
 
-    // need to make a axios call to render the users saved cards
-    // need to make a delete function to delete saved cards
 
-    // need to add mark-up and some styling
     return (
       <>
         <h1>User ID {id}</h1>
